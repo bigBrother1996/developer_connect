403583da063ded8517d84ea2afefa05ce866f324
Islombek <islombekhamidov1996@gmail.com>
2022-06-27 19:24:18 +0500

    profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,78 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// Get  /route/api/profile
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// Get  /route/api/profile/me
 // private
 
-router.use("/", (req, res) => res.send("profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "User",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      res.status(400).json({ msg: "there is no profile for this user" });
+    }
+    res.send("profile route");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+// Get  /route/api/profile
+// create or update profile
+// private
+
+router.post(
+  "/",
+  auth,
+  [
+    check("status", "Status is required").not().isEmpty(),
+    check("skills", "Skills is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      twitter,
+      youtube,
+      facebook,
+      instagram,
+      linkedin,
+    } = req.body;
+  }
+);
+// build profile object
+const profileFields = {};
+profileFields.user = req.user.id;
+
+if (company) profileFields.company = company;
+if (website) profileFields.website = website;
+if (location) profileFields.location = location;
+if (bio) profileFields.bio = bio;
+if (status) profileFields.status = status;
+if (githubusername) profileFields.githubusername = githubusername;
+
+if (skills) {
+  profileFields.skills = skills.split(",").map((skill) => {
+    return skill.trim();
+  });
+}
 
 module.exports = router;
